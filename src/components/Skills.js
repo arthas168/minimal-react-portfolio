06106152ef266cca2332fc6e51da3fcaf6fe48cb
Pete <pavlovskipetko@abv.bg>
2020-12-12 14:26:29 +0200

    update numbers

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,7 +11,7 @@ export default function Skills() {
                     <Tag data-tip="js" name="JavaScript" isFirst={true} text="👨‍💻 My go-to programming language! I've been using JavaScript both for personal and professional projects ever since 2018.
                     I've worked with TypeScript, Nodejs & Express to build solid backend APIs. I've also used React and React Native to bring dynamic & polished UIs to life.
                     For my databases when working with JavaScript I've utilized MongoDB & Postgres. And finally, I have numerous personal projects using JavaScript which you can find on my GitHub repo page."/>
-                    <Tag name="Kotlin" text="👨‍💻 I've been utilizing Kotlin (alongside Gradle and Quarkus) with a Postgres database on a daily basis for the past 5 months to build secure, reliable & 
+                    <Tag name="Kotlin" text="👨‍💻 I've been utilizing Kotlin (alongside Gradle and Quarkus) with a Postgres database on a daily basis for the past 6 months to build secure, reliable & 
                     fast backend microservices."/>
                     <Tag name="Databases" isLast={true} text="👨‍💻 When it comes to databases, from 2017 until now I've mostly used MongoDB, Postgres and Google's Firestore Service. 
                     I also have experience with MySQL, Redis and AWS."/>
@@ -21,7 +21,7 @@ export default function Skills() {
                     <Tag name="Java" text="☕ Java is my first programming language! 
                     During my time in the Software University I studied the basics, fundamentals, and advanced concepts of programming and OOP principles with Java. I haven't used it professionally
                     since, but at work I use Kotlin daily, so I feel pretty comfortable with the JVC ecosystem. I've also used Spring, Maven and Hibernate with MySQL."/>
-                    <Tag name="Flutter" text="🐦 For the 2 months I've been using Flutter on a daily basis to build fast, dynamic & interactive mobile UIs for both iOS and Android. I also have a few personal
+                    <Tag name="Flutter" text="🐦 For the 3 months I've been using Flutter on a daily basis to build fast, dynamic & interactive mobile UIs for both iOS and Android. I also have a few personal
                     projects in progress with Flutter which you can find on my GitHub repo page."/>
                     <Tag name="Python" isLast={true} text="🐍 I haven't used Python professionally yet, but I've built several personal projects (some of which are on my GitHub repo page).
                     Python is my go-to language for writing small scripts and also learning cybersecurity and ethical hacking. I really want to start using the Django framework for web development."/>
