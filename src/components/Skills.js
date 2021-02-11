e0f776ba7d87a2ac78226b29cedac7660872383a
Pete <pavlovskipetko@abv.bg>
2021-02-11 09:43:24 +0200

    Fix margins of tags

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,11 +8,11 @@ export default function Skills() {
 
                 <h1>Highly skilled with: </h1>
                 <div className="tag_list">
-                    <Tag name="Kotlin" isFirst={true} text="👨‍💻 I've been utilizing Kotlin (alongside Gradle and Quarkus) with a Postgres database on a daily basis for the past 7 months to build secure, reliable & 
+                    <Tag name="Kotlin" position={'is_first'} text="👨‍💻 I've been utilizing Kotlin (alongside Gradle and Quarkus) with a Postgres database on a daily basis for the past 7 months to build secure, reliable & 
                     fast backend microservices."/>
                     <Tag name="Flutter" text="🐦 For the past 5 months I've been using Flutter on a daily basis to build fast, dynamic & interactive mobile UIs for both iOS and Android. I also have a few personal
                     projects in progress with Flutter which you can find on my GitHub repo page."/>
-                    <Tag data-tip="js" name="JavaScript" text="👨‍💻 I've been using JavaScript both for personal and professional projects ever since 2018.
+                    <Tag data-tip="js" position={'is_last'} name="JavaScript" text="👨‍💻 I've been using JavaScript both for personal and professional projects ever since 2018.
                     I've worked with TypeScript and Nodejs (with Express and MongoDB). I've also used React and React Native."/>
                 </div>
                 <h1>Good at: </h1>
@@ -21,7 +21,7 @@ export default function Skills() {
                     <Tag name="Java" text="☕ Java is my first programming language! 
                     During my time in the Software University I studied the basics, fundamentals, and advanced concepts of programming and OOP principles with Java. I haven't used it professionally
                     since, but at work I use Kotlin daily, so I feel pretty comfortable with the JVM ecosystem. I've also used Spring, Maven and Hibernate with MySQL."/>
-                    <Tag name="Python" isLast={true} text="🐍 I haven't used Python professionally yet, but I've built several personal projects (some of which are on my GitHub repo page).
+                    <Tag name="Python" position={'is_last'} text="🐍 I haven't used Python professionally yet, but I've built several personal projects (some of which are on my GitHub repo page).
                     Python is my go-to language for writing small scripts and also learning cybersecurity and ethical hacking. I really want to start using the Django framework for web development."/>
 
                 </div>
@@ -29,7 +29,7 @@ export default function Skills() {
                 <div className="tag_list">
                     <Tag name="Elm" text="🌲 I'm really keen on learning Elm, because it looks like a very interesting alternative to JS in terms of frontend development." />
                     <Tag name="Go" text="🐹 Go seems like a really efficient and fun language to learn." />
-                    <Tag name="Elixir" isLast={true} text="🧪 Elixir seems like a really fun to use language and I'd like to learn it to build distributed and fault-tolerant applications." />
+                    <Tag name="Elixir" position={'is_last'} text="🧪 Elixir seems like a really fun to use language and I'd like to learn it to build distributed and fault-tolerant applications." />
                 </div>
             </div>
         </>
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -3,12 +3,12 @@ import React from 'react'
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
-export default function Tag({ name, text, isFirst, isLast }) {
+export default function Tag({ name, text, position }) {
 
     return <Popup trigger={
-        <div className={`tag ${isLast && "last_item"} ${isFirst && "first_item"}`}>
+        <div className={`tag ${position}`}>
             <p>{name}</p>
-            {isFirst && <img className="shake-horizontal" src="../assets/push.png" alt="info-button"></img>}
+            {position === "is_first" && <img className="shake-horizontal" src="../assets/push.png" alt="info-button"></img>}
         </div>
     } position="bottom center">
         <div className="popup_content">
